fee9bc52bd0526755fb0ae27e6a1f526f73e48ae
Angel_Cortes(Mr.js) <is726442@iteso.mx>
2024-03-01 14:40:11 -0600

    Se creo la rama y tambien la estrucutra de un proyecto de Node y se completo las funciones de product.js en controllers

--- Back/app/index.js
+++ Back/app/index.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
+app.use(express.static('../Front'));
+
+app.get('/',(req,res)=>{
+    res.send('El servidor funciona');
+})
+
 module.exports = app;
 
 // const express = require('express');
--- Back/server.js
+++ Back/server.js
@@ -1,5 +1,7 @@
+//Recuerda que todas las rutas locales para llammarlas seran desde aqui
 const app = require('./app/index.js');
 const port = 3000;
+
 app.listen(port, () =>{
     console.log(`Servidor escuchando en el puerto ${port}`);
 });
